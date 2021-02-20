cd364d29c700fd53f99accf263bc47e92cc6a24c
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2021-02-20 15:55:52 +0100

    initial commit

---
+++ input.js
@@ -0,0 +1,26 @@
+const problem = {
+  numOfDifferentBooks: 6,
+  numOfLibraries: 2,
+  numOfDays: 7,
+  booksScores: [
+    1, 2, 3, 6, 5, 4
+  ],
+  libraries: [
+    {
+      numOfBooks: 5,
+      signupDuration: 2,
+      booksPerDay: 2,
+      booksIds: [
+        0, 1, 2, 3, 4
+      ]
+    },
+    {
+      numOfBooks: 4,
+      signupDuration: 3,
+      booksPerDay: 1,
+      booksIds: [
+        3, 2, 5, 0
+      ]
+    }
+  ]
+}
